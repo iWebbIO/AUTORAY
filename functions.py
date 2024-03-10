f0b8a955ab8325093492e2bb60f0fca0fb964b09
Webb <82875181+iWebbIO@users.noreply.github.com>
2024-03-10 15:53:53 +0330

    Add Autoray 0.0.0 code

---
+++ functions.py
@@ -0,0 +1,22 @@
+import re
+import random
+import string
+
+def generate(length):
+    characters = string.ascii_letters + string.digits
+    password = ''.join(random.choice(characters) for _ in range(length))
+    return password
+def days(s):
+  # define a dictionary of conversion factors
+  factors = {"d": 1, "m": 30, "y": 365}
+  # find the number and unit in the string
+  match = re.search(r"(\d+)(\w)", s)
+  if match:
+    # get the number and unit from the match object
+    num = int(match.group(1))
+    unit = match.group(2)
+    # multiply the number by the factor corresponding to the unit
+    return num * factors[unit]
+  else:
+    # return None if no match is found
+    return None
