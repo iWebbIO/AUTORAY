22498fb2fb78fd16121ecc08bf866a0e07e9ea56
Webb <82875181+iWebbIO@users.noreply.github.com>
2024-03-12 17:33:09 +0330

    Update to v1.0.0
    Minor bug fixes and features

--- AutoRAYhandler.py
+++ AutoRAYhandler.py
@@ -1,80 +1,70 @@
-import AutoRAYcore as tvc
-import functions
-import multiprocessing as mp
-from discord import SyncWebhook
 from flask import Flask, request, render_template_string
-import os
+from discord import SyncWebhook
 from datetime import datetime
-import pytz
-from datetime import datetime
-webhook = SyncWebhook.from_url("DiscordWebhookUrl")
-
-
-
-def get_iran_time():
-    # Set the time zone to Iran
-    iran_tz = pytz.timezone('Asia/Tehran')
-    
-    # Get the current time in Iran
-    current_time = datetime.now(iran_tz)
-    
-    # Format the time as "Day_Of_the_week Hour:Minute"
-    formatted_time = current_time.strftime("%A %H:%M")
-    
-    return formatted_time
-app = Flask(__name__)
-
-# Decorator to check if the request is from localhost
+import multiprocessing as mp
+import AutoRAYcore as tvc
 import random, string
+import functions as extra
+import pytz
+import os
 
+keysfile_path = "/Settings/keys"
+channelsfile_path = "/Settings/channels"
+webhook = None
 apiSecret = "ql1lGsB7TTO3TOOR2vRjaMgQi2DvmEWtngOkxNtFhTLQaDUne6sZvhRhD0jXUAKC0DtL9EW8fCZO5GdzHaIZyuBM2Re2OdYi"
 
-        #Public route
-@app.route('/createkey')
+def sendTo_webhook(msgcontent):
+    if webhook != None:
+        webhook.send(msgcontent)
+app = Flask(__name__)
+
+@app.route("/createkey")
 def createkey():
     try:
-        if request.args.get("pass") == apiSecret:
-            newkey = functions.generate(16)
-            with open("keys.txt", "a") as f:
+        if request.args.get("secret") == apiSecret:
+            newkey = extra.generate(16)
+            with open("Settings/keys", "a") as f:
                 f.write(newkey + "\n")
             f.close()
-            webhook.send(f"New key generated! :white_check_mark: \nKey: `{newkey}`\n{datetime.now()}")
-            #return f"Subscription created! :white_check_mark: \nKey: `{newkey}`\n\nMain links:\nNormal: `http://moriw.net.eu.org:2052/connect?key={newkey}#WebbTVC-vip`\nTunneled: `http://tunnel.moriw.net.eu.org:2052/connect?key={newkey}#WebbTVC-vip`\n\nBackup Link: `http://free-01.duckhost.pro:8428/connect?key={newkey}#WebbTVC-vip`"
+            sendTo_webhook(
+                f"New key generated! ✅ \nKey: `{newkey}`\n{datetime.now()}"
+            )
+            # return f"Subscription created! ✅ \nKey: `{newkey}`\n\nMain links:\nNormal: `http://moriw.net.eu.org:2052/connect?key={newkey}#WebbTVC-vip`\nTunneled: `http://tunnel.moriw.net.eu.org:2052/connect?key={newkey}#WebbTVC-vip`\n\nBackup Link: `http://free-01.duckhost.pro:8428/connect?key={newkey}#WebbTVC-vip`"
             return f"Subscription created! ✅ \nKey: `{newkey}`\n\nLink: `http://85.232.241.109:53275/connect?key={newkey}#WebbTVC-vip`\nBackup Link: `http://85.232.241.109:7242/connect?key={newkey}#WebbTVC-vip`"
         else:
             request.abort(404)
     except:
         request.abort(404)
+
+
 @app.route("/allkeys")
-def seeallkeysapi():
-    webhook.send("⚠️Warning!\nThe Allkeys route has been triggered")
+def returnallkeys():
+    sendTo_webhook("⚠️ Warning!\nThe Allkeys route has been triggered")
     if request.args.get("key") == apiSecret:
-        webhook.send("Access granted :white_check_mark: \n_")
+        sendTo_webhook("Access granted ✅\n|")
         allkeycontent = ""
-        with open("keys.txt", "r") as f:
+        with open("/Settings/keys", "r") as f:
             apiReturnContent = f.readlines()
         for i in apiReturnContent:
             allkeycontent += i
         return allkeycontent
     else:
-        webhook.send("Access denied!  <@&1197993368270143568>  \n_")
-@app.route('/ping')
+        sendTo_webhook("Access denied!\n|")
+
+
+@app.route("/ping")
 def apialiveping():
-    try:
-        if request.args.get('key') == apiSecret:
-            webhook.send("Status ping: SUCCUSS")
-            return "ALIVE"
-        else:
-            webhook.send("Status ping: INVALID_AUTH")
-    except:
-        webhook.send("Status ping: INTERNAL_ERR")
-@app.route('/delkey')
-def deletekeyfromfile():
-    if request.args.get('key') == apiSecret:
+    sendTo_webhook("Status ping\nReturned `ALIVE`")
+    return "ALIVE"
+
+
+@app.route("/delkey")
+def deletekey():
+    if request.args.get("secret") == apiSecret:
         with open("keys.txt", "r") as f:
             keyslist = [line.strip() for line in f]
             f.close()
-        targetKey = request.args.get('tkey')
+        targetKey = request.args.get("targetkey")
         restKeys = []
         resultcode = "Nothing removed"
         for i in keyslist:
@@ -82,28 +72,34 @@ def deletekeyfromfile():
                 restKeys.append(i)
             else:
                 resultcode = "SUCCUSS"
-                webhook.send(f"Key got removed! ⚠️\nKey: `{i}`\n{datetime.now}")
+                sendTo_webhook(f"Key got removed! ⚠️\nKey: `{i}`\n{datetime.now}")
         with open("keys.txt", "w") as f:
             for i in restKeys:
                 f.write(f"{i}\n")
             f.close()
         return resultcode
-@app.route('/addkey')
-def addnewkey():
+
+
+@app.route("/addkey")
+def appendkey():
     try:
-        if request.args.get('key') == apiSecret:
+        if request.args.get("secret") == apiSecret:
             with open("keys.txt", "a") as f:
-                f.write(request.args.get('tkey') + "\n")
+                f.write(request.args.get("targetkey") + "\n")
                 f.close()
-            webhook.send(f"Key got added!  <:yes:1197990451484053615> \nKey: `{request.args.get('tkey')}`\n{datetime.now}")
+            sendTo_webhook(
+                f"Key got added! ✅\nKey: `{request.args.get('tkey')}`\n{datetime.now}"
+            )
             return "SUCCUSS"
     except:
         return "Failed to add key"
-        webhook.send(f"Failed to add {request.args.get('key')} to .KEYS")
-@app.route('/connect')
+        sendTo_webhook(f"Failed to add {request.args.get('key')} to ")
+
+
+@app.route("/connect")
 def sub():
-    with open("keys.txt", "r") as f:
-        keyslist = [line.strip() for line in f]  # This removes any trailing newline characters
+    with open("/Settings/keys", "r") as f:
+        keyslist = [line.strip() for line in f]
     try:
         rqkey = request.args.get("key")
         if str(rqkey) in list(keyslist):
@@ -113,20 +109,33 @@ def sub():
 
             item_list = [[channel_id, int(count)] for channel_id, count in item_list]
             for i in item_list:
-                irancurrenttimeday = get_iran_time()
-                allconfigs += tvc.subscription(channel_name=i[0], config_count=i[1], config_name=f"@IranRAY_vpn | {irancurrenttimeday}") + "\n"
-            webhook.send(f"\nSuccussful update\nKey: `{rqkey}`\n{datetime.now()}")
+                currentdatetime = extra.get_current_time()
+                allconfigs += (
+                    tvc.subscription(
+                        channel_name=i[0],
+                        config_count=i[1],
+                        config_name=f"AutoRAY | {currentdatetime}",
+                    )
+                    + "\n"
+                )
+            sendTo_webhook(f"\nSuccussful update ✅\nKey: `{rqkey}`\n{datetime.now()}")
             return allconfigs
         else:
-            webhook.send(f"Failed update ⚠️\nReason: Invalid password\nKey: `{rqkey}`\n_")
+            sendTo_webhook(
+                f"Failed update ⚠️\nReason: Invalid password\nKey: `{rqkey}`\n_"
+            )
             return "vless://00000000@incorrectpasskey.xd:200?mode=gun&security=none&encryption=none&type=grpc&serviceName=#ERR: Invalid key | @Iranray_VPN"
     except:
-        webhook.send("Failed update ⚠️\nReason: Internal server error\n{datetime.now()}\n_")
+        sendTo_webhook(
+            "Failed update ⚠️\nReason: Internal server error\n{datetime.now()}\n_"
+        )
         return "vless://00000000@internalerror.xd:200?mode=gun&security=none&encryption=none&type=grpc&serviceName=#ERR: Internal error | @Iranray_VPN"
-@app.route('/partner')
+
+
+@app.route("/custom")
 def partnersub():
-    with open("keys.txt", "r") as f:
-        keyslist = [line.strip() for line in f]  # This removes any trailing newline characters
+    with open("/Settings/keys", "r") as f:
+        keyslist = [line.strip() for line in f]
     try:
         rqkey = request.args.get("key")
         rqname = request.args.get("name")
@@ -137,33 +146,45 @@ def partnersub():
 
             item_list = [[channel_id, int(count)] for channel_id, count in item_list]
             for i in item_list:
-                allconfigs += tvc.subscription(channel_name=i[0], config_count=i[1], config_name=rqname) + "\n"
-            webhookpartner.send(f"\nSuccussful update\nKey: `{rqkey}`\n\n{datetime.now()}\n|")
+                allconfigs += (
+                    tvc.subscription(
+                        channel_name=i[0], config_count=i[1], config_name=rqname
+                    )
+                    + "\n"
+                )
+            sendTo_webhook(
+                f"(CUSTOM) Succussful update ✅\nKey: `{rqkey}`\n\n{datetime.now()}\n|"
+            )
             return allconfigs
         else:
-            webhookpartner.send(f"Failed update (Partner Route)⚠️\nReason: Invalid password\nKey: `{rqkey}`\n_")
+            sendTo_webhook(
+                f"(CUSTOM) Failed update (Partner Route)⚠️\nReason: Invalid password\nKey: `{rqkey}`\n_"
+            )
             return "vless://00000000@incorrectpasskey.xd:200?mode=gun&security=none&encryption=none&type=grpc&serviceName=#ERR: Invalid key | @Iranray_VPN"
     except:
-        webhookpartner.send("Failed updat (Partner Route)e ⚠️\nReason: Internal server error\n{datetime.now()}\n_")
+        sendTo_webhook(
+            "(CUSTOM) Failed update (Partner Route)e ⚠️\nReason: Internal server error\n{datetime.now()}\n_"
+        )
         return "vless://00000000@internalerror.xd:200?mode=gun&security=none&encryption=none&type=grpc&serviceName=#ERR: Internal error | @Iranray_VPN"
-@app.route('/miner')
-def minrsub():
-    minerconfigs = tvc.subscription(channel_name="iranrayminer", config_count=15, config_name="W ? B B | Iranray Miner 1.0")
-    return minerconfigs
 
-#if __name__ == '__main__':
-#    app.run(host="0.0.0.0", port=2052)
-def startServer(port):
-    print(f"Starting server on port {port}")
-    app.run(host='0.0.0.0', port=port)
 
-if __name__ == '__main__':
-    print('Main Line Starting')
-    p = mp.Process(target=startServer, args=(7242,))
-    p.daemon = True
-    p.start()
-    p1 = mp.Process(target=startServer, args=(53275,))
-    p1.daemon = True
-    p1.start()
-    p.join()
-    p1.join()
+# Run the server
+
+## Single port mode (Default)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=2873)
+# ---------
+## Use the following code to run the AutoRAY server on multiple ports
+# def startServer(port):
+#     print(f"Starting server on port {port}")
+#     app.run(host="0.0.0.0", port=port)
+# if __name__ == "__main__":
+#     print("Main Line Starting")
+#     p = mp.Process(target=startServer, args=(7242,))
+#     p.daemon = True
+#     p.start()
+#     p1 = mp.Process(target=startServer, args=(53275,))
+#     p1.daemon = True
+#     p1.start()
+#     p.join()
+#     p1.join()
--- functions.py
+++ functions.py
@@ -1,22 +1,45 @@
+import datetime
+import pytz
 import re
 import random
 import string
 
+
+def create_file(filename):
+    with open(filename, "a") as f:
+        f.close()
+
+
 def generate(length):
     characters = string.ascii_letters + string.digits
-    password = ''.join(random.choice(characters) for _ in range(length))
+    password = "".join(random.choice(characters) for _ in range(length))
     return password
+
+
 def days(s):
-  # define a dictionary of conversion factors
-  factors = {"d": 1, "m": 30, "y": 365}
-  # find the number and unit in the string
-  match = re.search(r"(\d+)(\w)", s)
-  if match:
-    # get the number and unit from the match object
-    num = int(match.group(1))
-    unit = match.group(2)
-    # multiply the number by the factor corresponding to the unit
-    return num * factors[unit]
-  else:
-    # return None if no match is found
-    return None
+    # define a dictionary of conversion factors
+    factors = {"d": 1, "m": 30, "y": 365}
+    # find the number and unit in the string
+    match = re.search(r"(\d+)(\w)", s)
+    if match:
+        # get the number and unit from the match object
+        num = int(match.group(1))
+        unit = match.group(2)
+        # multiply the number by the factor corresponding to the unit
+        return num * factors[unit]
+    else:
+        # return None if no match is found
+        return None
+
+
+def get_current_time():
+    # Set the time zone to Iran
+    iran_tz = pytz.timezone("Asia/Tehran")
+
+    # Get the current time in Iran
+    current_time = datetime.now(iran_tz)
+
+    # Format the time as "Day_Of_the_week Hour:Minute"
+    formatted_time = current_time.strftime("%A %H:%M")
+
+    return formatted_time
